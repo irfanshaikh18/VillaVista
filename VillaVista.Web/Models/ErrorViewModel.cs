3788b16a1cbcd9e01b68493acd986d9001e70fab
shaikhirfanali <irfansk.dev@gmail.com>
2025-02-19 12:27:17 +0530

    initial commit

---
+++ VillaVista.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VillaVista.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
