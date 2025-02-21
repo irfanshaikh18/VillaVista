43d142305a09a3c32752868020cda4b0f0e256c8
shaikhirfanali <irfansk.se@gmail.com>
2025-02-21 10:18:52 +0530

    Display Villa List on UI

---
+++ VillaVista.Web/Controllers/VillaController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using VillaVista.Infrastructure.Data;
+
+namespace VillaVista.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public VillaController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Domain.Entities.Villa> villas = _dbContext.Villas.ToList();
+            return View(villas);
+        }
+    }
+}
