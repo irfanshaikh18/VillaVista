18f3e40d32f73a33c67f1cfa5f1ffc063e4a43b2
shaikhirfanali <irfansk.se@gmail.com>
2025-02-24 11:44:45 +0530

    Display Data Annotation

--- VillaVista.Domain/Entities/Villa.cs
+++ VillaVista.Domain/Entities/Villa.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace VillaVista.Domain.Entities
 {
     public class Villa
@@ -5,9 +7,11 @@ namespace VillaVista.Domain.Entities
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [Display(Name = "Price per night")]
         public double Price { get; set; }
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
+        [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? Created_Date { get; set; }
         public DateTime? Updated_Date { get; set; }
